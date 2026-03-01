390a59b24066b932d3df5b33507eb7c4ee6e351d
Muhammed Sinan <sinanmohammed429@gmail.com>
2026-03-01 17:47:50 +0530

    Replaced profile photo with 3D character

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Social from "@/components/Social";
-import { hover } from "framer-motion";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
@@ -11,45 +10,51 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between 
-        xl:pt-8 xl:pb-24">
+        <div
+          className="flex flex-col xl:flex-row items-center justify-between 
+        xl:pt-8 xl:pb-24"
+        >
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">‎ Data Scientist</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Muhammed Sinan</span>
-              </h1>
-              <p className="max-w-[500px] mb-9 text-white/80">
-                I am dedicated to transforming raw data into actionable insights and I am
-                proficient in Machine Learning, Python, SQL, and Power BI.
-              </p>
-              {/* btn and socials */}
-              <div className="flex flex-col xl:flex-row items-center gap-8 ">
-                <a href="/assets/cv.pdf" download="Muhammed_Sinan_CV.pdf">
-                  <Button variant="outline"
+              Hello I&apos;m <br />{" "}
+              <span className="text-accent">Muhammed Sinan</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am dedicated to transforming raw data into actionable insights
+              and I am proficient in Machine Learning, Python, SQL, and Power
+              BI.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8 ">
+              <a href="/assets/ccv.pdf" download="Muhammed_Sinan_CV.pdf">
+                <Button
+                  variant="outline"
                   size="lg"
                   className="uppercase flex items-center gap-2"
-                  >
-                    <span>Download CV</span>
-                    <FiDownload className="text-xl" />
-                  </Button>
-                </a>
-                <div className="mb-8 xl:mb-0">
-                  <Social
-                   containerStyles="flex gap-6"
-                    iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary
-                    hover:text-primary hover:transition-all duration-500" />
-                  </div>
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary
+                    hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
-            {/* photo */}
+          </div>
+          {/* photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
-            </div>
+          </div>
         </div>
       </div>
       <Stats />
     </section>
   );
 };
-  export default Home;
+export default Home;
